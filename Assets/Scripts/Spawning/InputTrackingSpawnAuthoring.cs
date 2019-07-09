4939978c2c2741b749931c1c532ed6af228efeb2
Wessam Ibrahim <wessami@lana.games>
2019-07-09 20:22:37 +0200

    Spawn entity / components fo rinput tracking

---
+++ Assets/Scripts/Spawning/InputTrackingSpawnAuthoring.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using Unity.Entities;
+using System.Collections.Generic;
+
+public class InputTrackingSpawnAuthoring : MonoBehaviour, IConvertGameObjectToEntity, IDeclareReferencedPrefabs
+{
+	public GameObject Prefab;
+
+	public void DeclareReferencedPrefabs(List<GameObject> gameObjects)
+	{
+		gameObjects.Add(Prefab);
+	}
+
+	public void Convert(Entity entity, EntityManager dstManager, GameObjectConversionSystem conversionSystem)
+	{
+		var spawnerData = new InputTrackingSpawn
+		{
+			// The referenced prefab will be converted due to DeclareReferencedPrefabs.
+			// So here we simply map the game object to an entity reference to that prefab.
+			Prefab = conversionSystem.GetPrimaryEntity(Prefab)
+		};
+		dstManager.AddComponentData(entity, spawnerData);
+	}
+}
---
+++ Assets/Scripts/Spawning/InputTrackingSpawnSystem.cs
@@ -0,0 +1,37 @@
+﻿using Unity.Collections;
+using Unity.Entities;
+
+public class InputTrackingSpawnSystem : ComponentSystem
+{
+	EntityQuery m_SpawnerQuery;
+
+	protected override void OnCreate()
+	{
+		m_SpawnerQuery = GetEntityQuery
+		(
+			ComponentType.ReadOnly<InputTrackingSpawn>()
+		);
+	}
+
+	protected override void OnUpdate()
+	{
+		var spawnerFromEntities = m_SpawnerQuery.ToComponentDataArray<InputTrackingSpawn>(Allocator.TempJob);
+		var entities = m_SpawnerQuery.ToEntityArray(Allocator.TempJob);
+
+		if (entities.Length > 0)
+		{
+			var instance = EntityManager.Instantiate(spawnerFromEntities[0].Prefab);
+			EntityManager.AddComponent(instance, typeof(InputLocations));
+			EntityManager.AddComponent(instance, typeof(InputTimes));
+			EntityManager.AddComponent(instance, typeof(InputEvents));
+			EntityManager.AddComponent(instance, typeof(ProjectedInputs));
+
+			EntityManager.DestroyEntity(entities[0]);
+		}
+
+		entities.Dispose();
+		spawnerFromEntities.Dispose();
+
+		Enabled = false;
+	}
+}
