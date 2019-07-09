2c1319bb3962f86702a19f59cd4b88577ea065cf
Wessam Ibrahim <wessami@lana.games>
2019-07-09 20:22:26 +0200

    Components to save input data

---
+++ Assets/Scripts/Input/InputTracking.cs
@@ -0,0 +1,28 @@
+﻿using Unity.Entities;
+using Unity.Mathematics;
+
+public struct InputLocations : IComponentData
+{
+	public float2 Current;
+	public float2 LastPrimaryDown;
+	public float2 LastPrimaryUp;
+}
+
+public struct InputTimes : IComponentData
+{
+	public float LastPrimaryDown;
+	public float LastPrimaryUp;
+}
+
+public struct InputEvents : IComponentData
+{
+	public bool PrimaryDown;
+	public bool PrimaryUp;
+}
+
+public struct ProjectedInputs : IComponentData
+{
+	public float2 Current;
+	public float2 LastPrimaryDown;
+	public float2 LastPrimaryUp;
+}
