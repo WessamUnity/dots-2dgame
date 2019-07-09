ee5d9bdbc4df414a436227485dacfb2fc1871a5f
Wessam Ibrahim <wessami@lana.games>
2019-07-09 18:13:27 +0200

    Use LevelSize

--- Assets/Scripts/Spawning/LevelSpawnSystem.cs
+++ Assets/Scripts/Spawning/LevelSpawnSystem.cs
@@ -5,31 +5,31 @@ using Unity.Transforms;
 
 public class LevelSpawnSystem : ComponentSystem
 {
-    EntityQuery m_SpawnerQuery;
+    EntityQuery m_LevelSpawnerQuery;
 
-    protected override void OnCreate()
+	protected override void OnCreate()
     {
-        m_SpawnerQuery = GetEntityQuery
-             (
-                 ComponentType.ReadOnly<LevelSpawn>(),
-                 ComponentType.ReadWrite<LocalToWorld>()
-             );
-    }
+		m_LevelSpawnerQuery = GetEntityQuery
+		(
+			ComponentType.ReadOnly<LevelSpawn>(),
+			ComponentType.ReadWrite<LocalToWorld>()
+		);
+	}
 
     protected override void OnUpdate()
     {
-        var spawnerFromEntities = m_SpawnerQuery.ToComponentDataArray<LevelSpawn>(Allocator.TempJob);
-        var entities = m_SpawnerQuery.ToEntityArray(Allocator.TempJob);
+        var levelSpawners = m_LevelSpawnerQuery.ToComponentDataArray<LevelSpawn>(Allocator.TempJob);
+        var entities = m_LevelSpawnerQuery.ToEntityArray(Allocator.TempJob);
 
 		if (entities.Length > 0)
 		{
-			Spawn(entities[0], spawnerFromEntities[0]);
+			Spawn(entities[0], levelSpawners[0]);
 		}
 
         entities.Dispose();
-        spawnerFromEntities.Dispose();
+		levelSpawners.Dispose();
 
-        Enabled = false;
+		Enabled = false;
     }
 
     void Spawn(Entity entity, LevelSpawn levelSpawnFromEntity)
@@ -37,6 +37,7 @@ public class LevelSpawnSystem : ComponentSystem
         // Create our new Bot entity
         var instance = EntityManager.Instantiate(levelSpawnFromEntity.Prefab);
         EntityManager.SetComponentData(instance, new Translation { Value = new float3() });
+		EntityManager.AddComponentData(instance, new LevelSize { X = levelSpawnFromEntity.Size.x, Y = levelSpawnFromEntity.Size.y });
 
         EntityManager.DestroyEntity(entity);
     }
