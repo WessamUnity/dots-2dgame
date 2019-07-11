aa5e85823160672b18168ac6f72d5b1e1ba7bbf3
Wessam Ibrahim <wessami@lana.games>
2019-07-11 18:25:46 +0200

    System to move projectiles

---
+++ Assets/Scripts/Projectile/ProjectileMoverSystem.cs
@@ -0,0 +1,32 @@
+﻿using Unity.Burst;
+using Unity.Entities;
+using Unity.Jobs;
+using Unity.Mathematics;
+using Unity.Transforms;
+using UnityEngine;
+
+public class ProjectileMoverSystem : JobComponentSystem
+{
+	[BurstCompile]
+	[RequireComponentTag(typeof(ProjectileMovingTag))]
+	struct ProjectileMoverJob : IJobForEach<ProjectileSpeed, Translation, Rotation>
+	{
+		public float DeltaTime;
+
+		public void Execute(ref ProjectileSpeed speed, ref Translation translation, ref Rotation rotation)
+		{
+			translation.Value += math.forward(rotation.Value) * speed.MetersPerSecond * DeltaTime;
+		}
+	}
+
+	protected override JobHandle OnUpdate(JobHandle inputDeps)
+	{
+		var job = new ProjectileMoverJob
+		{
+			DeltaTime = Time.deltaTime
+		};
+
+		inputDeps = job.Schedule(this, inputDeps);
+		return inputDeps;
+	}
+}
